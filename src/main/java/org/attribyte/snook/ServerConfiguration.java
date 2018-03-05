006e575519398da4de94e4f7cd7976df4b29925d
Matt Hamer <matt@attribyte.com>
2018-03-05 12:55:05 -0600

    Typo

--- src/main/java/org/attribyte/snook/ServerConfiguration.java
+++ src/main/java/org/attribyte/snook/ServerConfiguration.java
@@ -162,8 +162,6 @@ public class ServerConfiguration {
          if(!keyStoreProvider.isEmpty()) {
             contextFactory.setKeyStoreProvider(keyStoreProvider);
          }
-
-
          this.sslContextFactory = Optional.of(contextFactory);
       } else {
          this.sslContextFactory = Optional.empty();
@@ -171,7 +169,7 @@ public class ServerConfiguration {
 
       this.connectionSecurity = ConnectionSecurity.fromString(init.getProperty(CONNECTION_SECURITY_PROPERTY, DEFAULT_CONNECTION_SECURITY));
       if(connectionSecurity != ConnectionSecurity.NONE && !sslContextFactory.isPresent()) {
-         throw new InitializationException(String.format("A 'keystore.File' must be specified for 'connectionSecurity', %s", connectionSecurity));
+         throw new InitializationException(String.format("A 'keystore.File' must be specified with 'connectionSecurity', %s", connectionSecurity));
       }
    }
 
